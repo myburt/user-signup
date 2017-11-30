93add2ca17b318de749dc5f8716222e4b93859a1
Mike Burt <myburt@hotmail.com>
2017-11-30 10:24:32 -0600

    Complete working form with redirects

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, redirect, render_template
-import re
 import cgi
 import os
 import jinja2
@@ -17,7 +16,7 @@ def index():
     #if there is an error
     
 
-    return render_template("sample.html")
+    return render_template("sample.html", username='')
 
 @app.route("/", methods=["POST"])
 def index_post():
@@ -26,21 +25,72 @@ def index_post():
     verify = request.form['verify']
     email = request.form['email']
 
-    user_re = re.compile('\w-')
+    username_error = ""
+    password_error = ""
+    verify_error = ""
+    email_error = ""
 
     #The user leaves any of the following fields empty: username, password, verify password.
     if (not username):# or (not password) or (not verify):
         username_error = "You must enter a username"
-    elif not re.match(user_re, username):
-        username_error = "Username is invalid please reenter"
-        
-    #The user's username or password is not valid -- for example, it contains a space character 
-    # or it consists of less than 3 characters or more than 20 characters (e.g., a username or password of "me" would be invalid).
 
+    username_error = is_valid_field(username, " ~^*") #checks for valid username
+    password_error = is_valid_field(password, " ~^*") #checks for valid password
+    
     #The user's password and password-confirmation do not match.
+    if len(password) > 1 and password != verify:
+        verify_error = "Password and verify password must match"
 
     #The user provides an email, but it's not a valid email.
     #valid email  has a single @, a single ., contains no spaces, and is between 3 and 20 characters long.
-    return render_template("sample.html", username_error=username_error)
+    if email:
+        email_error = is_valid_email(email)
 
+    if username_error or password_error or verify_error or email_error:
+        if username_error:
+            username = ""
+        if password_error:
+            password = ""
+            verify = ""
+        if email and email_error:
+            email = ""
+        return render_template("sample.html", username=username, password=password, verify=verify,email=email,
+                                              username_error=username_error, password_error =  password_error,
+                                              verify_error=verify_error, email_error=email_error)
+    else:
+        return redirect("/valid-signup?username={0}".format(username))
+
+def is_valid_field(test_string, test_for):
+    if len(test_string) < 3:
+        return "This must be at least 3 characters long"
+    if len(test_string) > 20:
+        return "this can't be more than 20 characters long."
+
+    for letter in test_string:
+        if letter in test_for:
+            return "This field cannot contain spaces or any of theese symbols '~^*"
+
+    return ""
+
+def is_valid_email (test_string):
+    email_error = is_valid_field(test_string, " ~^")
+    has_dot = False
+    has_at = False
+
+    if not email_error:
+        for letter in test_string:
+            if letter == ".":
+                has_dot = True
+            if letter == "@":
+                has_at = True
+        
+    if has_dot and has_at:
+        return ""
+    else:
+        return "Please make sure you email fits the pattern 'person@place.thing'"
+
+@app.route("/valid-signup")
+def valid_setup():
+    username = request.args.get("username")
+    return render_template("valid-signup.html", username=username)
 app.run()
